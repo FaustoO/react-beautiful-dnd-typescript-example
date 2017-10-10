327790fec19fe76be6684fee18a83937193b1e1f
RainerAtSpirit <R.Wittmann@spirit.de>
2017-10-10 12:18:17 +0200

    add flow example and convert to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,118 @@
 import * as React from 'react';
-import './App.css';
+import { Component } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import {
+  DraggableStyle,
+  DroppableProvided,
+  DroppableStateSnapshot,
+  DraggableProvided,
+  DraggableStateSnapshot,
+  DropResult,
+} from 'react-beautiful-dnd';
 
-const logo = require('./logo.svg');
+type Item = {
+  id: string,
+  content: string,
+};
 
-class App extends React.Component {
+type State = {
+  items: Item[]
+};
+
+// fake data generator
+const getItems = (count: number): Item[] =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    content: `item ${k}`
+  }));
+
+// a little function to help us with reordering the result
+const reorder = (list: Item[], startIndex: number, endIndex: number) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (draggableStyle: DraggableStyle, isDragging: boolean): Object => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'grey',
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+
+  margin: draggableStyle && draggableStyle.margin ? draggableStyle.margin : `0 0 ${grid}px 0`,
+});
+
+const getListStyle = (isDraggingOver: boolean) => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: grid,
+  width: 250
+});
+
+class App extends Component {
+  state: State  = {
+    items: getItems(10),
+  };
+
+  onDragEnd = (result: DropResult) => {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items
+    });
+  }
+
+  // Normally you would want to split things out into separate components.
+  // But in this example everything is just done in one place for simplicity
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(dropProvided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
+            <div
+              ref={dropProvided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {this.state.items.map(item => (
+                <Draggable key={item.id} draggableId={item.id}>
+                  {(provided: DraggableProvided, itemSnapshot: DraggableStateSnapshot) => (
+                    <div>
+                      <div
+                        ref={provided.innerRef}
+                        style={getItemStyle(
+                          provided.draggableStyle,
+                          itemSnapshot.isDragging
+                        )}
+                        {...provided.dragHandleProps}
+                      >
+                        {item.content}
+                      </div>
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     );
   }
 }
