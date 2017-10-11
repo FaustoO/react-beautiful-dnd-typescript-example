7880faffc9ad54f0958030986723e50fdbd44df0
RainerAtSpirit <R.Wittmann@spirit.de>
2017-10-11 16:40:26 +0200

    update example to use react-beautiful-dnd-tests.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
 import * as React from 'react';
-import { Component } from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import {
-  DraggableStyle,
-  DroppableProvided,
-  DroppableStateSnapshot,
-  DraggableProvided,
-  DraggableStateSnapshot,
-  DropResult,
-} from 'react-beautiful-dnd';
+import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
 
-type Item = {
-  id: string,
-  content: string,
+interface Item {
+  id: string;
+  content: string;
+}
+
+const getItems = (count: number): Item[] => {
+  return Array
+      .from({length: count}, (v, k) => k)
+      .map(k => ({
+        id: `item-${k}`,
+        content: `item ${k}`
+      }));
 };
 
-type State = {
-  items: Item[]
-};
-
-// fake data generator
-const getItems = (count: number): Item[] =>
-  Array.from({ length: count }, (v, k) => k).map(k => ({
-    id: `item-${k}`,
-    content: `item ${k}`
-  }));
-
-// a little function to help us with reordering the result
-const reorder = (list: Item[], startIndex: number, endIndex: number) => {
+const reorder = (list: any[], startIndex: number, endIndex: number) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
@@ -35,86 +23,79 @@ const reorder = (list: Item[], startIndex: number, endIndex: number) => {
   return result;
 };
 
-const grid = 8;
-
-const getItemStyle = (draggableStyle: DraggableStyle, isDragging: boolean): Object => ({
-  // some basic styles to make the items look a bit nicer
+const getItemStyle = (draggableStyle: any, isDragging: any) => ({
   userSelect: 'none',
-  padding: grid * 2,
-
-  // change background colour if dragging
   background: isDragging ? 'lightgreen' : 'grey',
-
-  // styles we need to apply on draggables
-  ...draggableStyle,
-
-  margin: draggableStyle && draggableStyle.margin ? draggableStyle.margin : `0 0 ${grid}px 0`,
+  ...draggableStyle
 });
 
-const getListStyle = (isDraggingOver: boolean) => ({
+const getListStyle = (isDraggingOver: any) => ({
   background: isDraggingOver ? 'lightblue' : 'lightgrey',
-  padding: grid,
   width: 250
 });
 
-class App extends Component {
-  state: State  = {
-    items: getItems(10),
-  };
+interface AppState {
+  items: Item[];
+}
 
-  onDragEnd = (result: DropResult) => {
-    // dropped outside the list
+export default class App extends React.Component<{}, AppState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      items: getItems(10)
+    };
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
+
+  onDragEnd(result: DropResult) {
     if (!result.destination) {
       return;
     }
 
     const items = reorder(
-      this.state.items,
-      result.source.index,
-      result.destination.index
+        this.state.items,
+        result.source.index,
+        result.destination.index
     );
 
-    this.setState({
-      items
-    });
+    this.setState({items});
   }
 
-  // Normally you would want to split things out into separate components.
-  // But in this example everything is just done in one place for simplicity
   render() {
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <Droppable droppableId="droppable">
-          {(dropProvided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
-            <div
-              ref={dropProvided.innerRef}
-              style={getListStyle(snapshot.isDraggingOver)}
-            >
-              {this.state.items.map(item => (
-                <Draggable key={item.id} draggableId={item.id}>
-                  {(provided: DraggableProvided, itemSnapshot: DraggableStateSnapshot) => (
-                    <div>
-                      <div
-                        ref={provided.innerRef}
-                        style={getItemStyle(
-                          provided.draggableStyle,
-                          itemSnapshot.isDragging
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+                <div
+                    ref={provided.innerRef}
+                    style={getListStyle(snapshot.isDraggingOver)}
+                >
+                  {this.state.items.map(item => (
+                      <Draggable key={item.id} draggableId={item.id}>
+                        {// tslint:disable-next-line:no-shadowed-variable
+                        (provided, snapshot) => (
+                            <div>
+                              <div
+                                  ref={provided.innerRef}
+                                  style={getItemStyle(
+                                      provided.draggableStyle,
+                                      snapshot.isDragging
+                                  )}
+                                  {...provided.dragHandleProps}
+                              >
+                                {item.content}
+                              </div>
+                              {provided.placeholder}
+                            </div>
                         )}
-                        {...provided.dragHandleProps}
-                      >
-                        {item.content}
-                      </div>
-                      {provided.placeholder}
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+                      </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+            )}
+          </Droppable>
+        </DragDropContext>
     );
   }
 }
-
-export default App;
